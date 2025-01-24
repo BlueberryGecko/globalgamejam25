34b9478fb26d6f9c7643068b1e9c629c730884b2
Matteo Bossi <matt.bossi@bluewin.ch>
2025-01-24 22:21:58 +0100

    add configurable player movement

---
+++ Player.cs
@@ -0,0 +1,39 @@
+using Godot;
+
+namespace Globalgamejam25;
+
+public partial class Player : Node2D
+{
+	private Vector2 velocity;
+	[Export]
+	private float maxSpeed = 300;
+	[Export]
+	private float acceleration = 1500;
+	[Export]
+	private float deceleration = 1f / 5f;
+	
+	public override void _Process(double delta)
+	{
+		var move = new Vector2();
+		
+		if (Input.IsActionPressed("move_up"))
+			move.Y -= 1;
+		if (Input.IsActionPressed("move_down"))
+			move.Y += 1;
+		if (Input.IsActionPressed("move_left"))
+			move.X -= 1;
+		if (Input.IsActionPressed("move_right"))
+			move.X += 1;
+        
+		move = move.Normalized() * acceleration * (float)delta;
+
+		if ((move.X >= 0 && velocity.X <= 0) || (move.X <= 0 && velocity.X >= 0))
+			move.X += -velocity.X / deceleration * (float)delta;
+		if ((move.Y >= 0 && velocity.Y <= 0) || (move.Y <= 0 && velocity.Y >= 0))
+			move.Y += -velocity.Y / deceleration * (float)delta;
+
+		velocity += move;
+		velocity = velocity.LimitLength(maxSpeed);
+		Position += velocity * (float)delta;
+	}
+}
